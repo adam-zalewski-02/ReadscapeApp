f136b9d9d9cc86364aaf95333f86191f748fbe29
Adam Zalewski <VisterPL23@gmail.com>
2023-11-06 19:13:36 +0100

    add Volume class

---
+++ core/model/src/main/java/com/example/model/book/Volume.kt
@@ -0,0 +1,14 @@
+package com.example.model.book
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Volume(
+    val id: String,
+    val kind: String,
+    val etag: String,
+    val selfLink: String,
+    val volumeInfo: VolumeInfo,
+    val saleInfo: SaleInfo,
+    val accessInfo: AccessInfo
+)
