5081d1aa0877248c749b52038079992d0ca2601c
Adam Zalewski <VisterPL23@gmail.com>
2023-11-07 02:19:04 +0100

    work on BookShopScreen

--- feature/bookshop/src/main/java/com/example/bookshop/BookShopScreen.kt
+++ feature/bookshop/src/main/java/com/example/bookshop/BookShopScreen.kt
@@ -1,4 +1,29 @@
 package com.example.bookshop
 
-class BookShopScreen {
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+internal fun BookShopRoute(
+    onBookClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewmodel: BookShopViewModel = hiltViewModel()
+) {
+    val bookState by viewmodel.books.collectAsState()
+
+    BookShopScreen(
+        onBookClick = onBookClick,
+        modifier = modifier
+    )
+}
+
+@Composable
+internal fun BookShopScreen(
+    onBookClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
 }
