61b2c56355a99cb5d6599612b1e2844b89c19a60
Adam Zalewski <VisterPL23@gmail.com>
2023-12-27 17:09:00 +0100

    add UserSearchResult class

--- core/model/src/main/java/com/example/model/SearchResult.kt
+++ core/model/src/main/java/com/example/model/SearchResult.kt
@@ -2,6 +2,7 @@ package com.example.model
 
 import com.example.model.book.Volume
 
+/** An entity that holds the search result */
 data class SearchResult (
     val volumes: List<Volume> = emptyList(),
 )
